d8736be326f8023ba19d0febdc46f767b23e2e61
Leonardo Lucas <leonardolucas2020@gmail.com>
2024-10-30 10:23:17 -0400

    :tada: Initial commit

---
+++ src/main/java/com/petsemfronteiras/CadastroDePets/CadastroDePetsApplication.java
@@ -0,0 +1,13 @@
+package com.petsemfronteiras.CadastroDePets;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadastroDePetsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CadastroDePetsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/petsemfronteiras/CadastroDePets/CadastroDePetsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.petsemfronteiras.CadastroDePets;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadastroDePetsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
