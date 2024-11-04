ddbaa0de75466616e1ccbcdc2def9ef8020c73d0
Leonardo Lucas <leonardolucas2020@gmail.com>
2024-11-04 11:33:18 -0400

    :wrench: feat: Add `Lombok` in classes to abstraction

--- src/main/java/com/petsemfronteiras/CadastroDePets/Animais/Model/AnimalModel.java
+++ src/main/java/com/petsemfronteiras/CadastroDePets/Animais/Model/AnimalModel.java
@@ -3,11 +3,18 @@ package com.petsemfronteiras.CadastroDePets.Animais.Model;
 
 import com.petsemfronteiras.CadastroDePets.Vacinas.Model.VacinaModel;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
 @Entity
 @Table(name = "tb_cadastro")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+
 public class AnimalModel {
 
     @Id
@@ -23,73 +30,4 @@ public class AnimalModel {
     @OneToMany(mappedBy = "animal")
     private List<VacinaModel> vacinas;
 
-
-    public AnimalModel() {
-    }
-
-    public AnimalModel(String nome, String cor, String especie, String raca, int idade, String sexo, List<VacinaModel> vacinas) {
-        this.nome = nome;
-        this.cor = cor;
-        this.especie = especie;
-        this.raca = raca;
-        this.idade = idade;
-        this.sexo = sexo;
-        this.vacinas = vacinas;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public String getCor() {
-        return cor;
-    }
-
-    public void setCor(String cor) {
-        this.cor = cor;
-    }
-
-    public String getEspecie() {
-        return especie;
-    }
-
-    public void setEspecie(String especie) {
-        this.especie = especie;
-    }
-
-    public String getRaca() {
-        return raca;
-    }
-
-    public void setRaca(String raca) {
-        this.raca = raca;
-    }
-
-    public int getIdade() {
-        return idade;
-    }
-
-    public void setIdade(int idade) {
-        this.idade = idade;
-    }
-
-    public String getSexo() {
-        return sexo;
-    }
-
-    public void setSexo(String sexo) {
-        this.sexo = sexo;
-    }
-
-    public List<VacinaModel> getVacinas() {
-        return vacinas;
-    }
-
-    public void setVacinas(List<VacinaModel> vacinas) {
-        this.vacinas = vacinas;
-    }
 }
--- src/main/java/com/petsemfronteiras/CadastroDePets/Vacinas/Model/VacinaModel.java
+++ src/main/java/com/petsemfronteiras/CadastroDePets/Vacinas/Model/VacinaModel.java
@@ -2,9 +2,16 @@ package com.petsemfronteiras.CadastroDePets.Vacinas.Model;
 
 import com.petsemfronteiras.CadastroDePets.Animais.Model.AnimalModel;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Table(name = "tb_vacinas")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+
 public class VacinaModel {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -17,37 +24,4 @@ public class VacinaModel {
     private AnimalModel animal;
 
 
-
-    public VacinaModel() {
-    }
-
-    public VacinaModel(String nome, String marca, AnimalModel animal) {
-        this.nome = nome;
-        this.marca = marca;
-        this.animal = animal;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public String getMarca() {
-        return marca;
-    }
-
-    public void setMarca(String marca) {
-        this.marca = marca;
-    }
-
-    public AnimalModel getAnimal() {
-        return animal;
-    }
-
-    public void setAnimal(AnimalModel animal) {
-        this.animal = animal;
-    }
 }
